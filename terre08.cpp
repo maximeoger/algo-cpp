2597b150e34a30e4cff6f1a7533da2bc72d5747d
maximeoger <maximeoger93@proton.me>
2023-04-29 17:59:51 +0200

    terre08

--- terre08.cpp
+++ terre08.cpp
@@ -2,18 +2,15 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-  if(argc > 2 || argc == 1) {
+  if(
+    argc > 2 || 
+    argc == 1 
+  ) {
     cerr << "Erreur." << endl;
     return 1;
   }
 
-  int len = 0;
-  
-  for(int i=0; i<(argc-1); i++) {
-    len = len + string(argv[i]).length();
-  }
-
-  cout << len << endl;
+  cout << string(argv[1]).length() << endl;
   
   return 0;
-}
+} 
