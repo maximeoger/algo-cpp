0979f3c1d7823a49e4e4e0ce8301861fbfc0f9e7
maximeoger <maximeoger93@proton.me>
2023-04-30 18:52:08 +0200

    terre09

---
+++ terre09.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main (int argc, char* argv[]) {
+  if(argc == 1 || argc < 3) {
+    cerr << "Veuillez entrer un nombre en premier argument et une puissance en second argument." << endl;
+    return 1;
+  }
+
+  if(argc > 3) {
+    cerr << "Vous avez entre trop de nombre. Veuillez entre deux nombre, le nombre de base et sa puissance." << endl;
+    return 1;
+  }
+
+  if(atof(argv[2]) < 0) {
+    cerr << "Veuillez entrer une puissance positive ou egale a zero." << endl;
+    return 1;
+  }
+
+  double x = atof(argv[1]), y = atof(argv[2]);
+
+  double result = pow(x, y);
+
+  cout << result << endl;
+
+  return 0;
+}
