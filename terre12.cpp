c56d213244af9cbdc90e1aea324603d8e23c48f1
maximeoger <maximeoger93@proton.me>
2023-05-11 10:00:27 +0200

    terre12

--- terre12.cpp
+++ terre12.cpp
@@ -1,29 +1,57 @@
 #include <iostream>
+
 using namespace std;
 
 
+/*
+  24 to 12 : 
+  Convertir une heure au format 24h vers le format 12h
+*/
 
-bool inputCorrectlyFormatted(str input){
-  char* data;
-  char separator = ':';
 
-  bool lengthIsCorrect = input.length() == 5;
-  bool separatorIsColon = input[2] == separator;
+int main(int argc, char* argv[]) 
+{
+  if(argc == 1) {
+    cerr << "Veuillez entrer une heure a convertir" << endl;
+    return 1;
+  }
 
-  // separer la string a partir d'un delimiteur
-  data = strtok(input, separator)
+  if(argc > 2) {
+    cerr << "Vous avez fournis trop d'arguments" << endl;
+    return 1;
+  }
 
-  int hours = data[0];
-  int mins = data[1];
+  string arg = argv[1];
+  string h, m;
+
+  for(int i=0; i<arg.length(); i++) {
+    char num = arg[i];
+    if(i < 2) {
+      h = h + num;
+    }
+    if(i > 2) {
+      m = m + num;
+    }
+  }
+
+  int h_int = stoi(h);
+  int m_int = stoi(m);
+
+  if((h_int == 0) && (m_int <= 59)) {
+    cout << h << ":" << m << "AM" << endl;
+  }
   
-  bool hoursFormatIsCorrect = hours >= 0 && hours <= 24;
-  bool minsFormatIsCorrect = mins >= 0 && mins <= 60;
+  if((h_int >= 1) && (h_int <= 11)) {
+    cout << h_int << ":" << m << "AM" << endl;
+  }
 
-}
+  if((h_int == 12) && (m_int >= 0 && m_int <= 59)) {
+    cout << h << ":" << m << "PM" << endl;
+  }
 
-int main(int argc, char* argv[]) {
-  
-  
+  if((h_int >= 13 && h_int <= 23) && (m_int >= 0 && m_int <= 59)) {
+    cout << h_int - 12 << ":" << m << "PM" << endl;
+  }
   
   return 0;
 }
